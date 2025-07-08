68363ec0bced29bf9a0978ad12497473c26c73b4
France020800 <francescomarchini7@gmail.com>
2025-07-08 09:53:04 +0200

    remove redundant functions

--- eval.py
+++ eval.py
@@ -1,7 +1,5 @@
 import torch
 import numpy as np
-from PIL import Image
-from torchvision.transforms import transforms
 from tqdm import tqdm
 from geopy.distance import geodesic as GD
 
@@ -62,50 +60,6 @@ def single_distance_accuracy(target, pred, dis=2500, gps_gallery=None):
     return correct
 
 
-'''def eval_images(val_dataloader, model, device="cpu"):
-    model.eval()
-    preds = []
-    targets = []
-
-    gps_gallery = model.gps_gallery.to(device)
-
-
-    with torch.no_grad():
-        for imgs, labels in tqdm(val_dataloader, desc="Evaluating"):
-            labels = labels.cpu().numpy()
-            imgs = imgs.to(device)
-
-            # Get predictions (probabilities for each location based on similarity)
-            logits_per_image = model(imgs, gps_gallery)
-            probs = logits_per_image.softmax(dim=-1)
-
-            # Predict gps location with the highest probability (index)
-            # outs = torch.argmax(probs, dim=-1).detach().cpu().numpy()
-
-            # My implementation
-            outs = []
-            for prob_row in probs:
-                out = most_probable_frequent_location(prob_row, gps_gallery)
-                outs.append(out)
-
-            preds.append(outs)
-            targets.append(labels)
-
-    preds = np.concatenate(preds, axis=0)
-    targets = np.concatenate(targets, axis=0)
-
-    model.train()
-
-    distance_thresholds = [2500, 750, 200, 25, 1]  # km
-    accuracy_results = {}
-    for dis in distance_thresholds:
-        acc, avg_distance_error = distance_accuracy(targets, preds, dis, gps_gallery)
-        print(f"Accuracy at {dis} km: {acc}, Average Distance Error: {avg_distance_error}")
-        accuracy_results[f'acc_{dis}_km'] = acc
-
-    return accuracy_results
-'''
-
 def eval_images(val_dataloader, model, gps_gallery, device="cpu"):
     model.eval()
     preds = []
@@ -142,45 +96,3 @@ def eval_images(val_dataloader, model, gps_gallery, device="cpu"):
         accuracy_results[f'acc_{dis}_km'] = acc
 
     return accuracy_results
-
-'''def new_eval_images(val_dataloader, model, device="cpu"):
-    model.eval()
-    preds = []
-    targets = []
-
-    gps_gallery = model.gps_gallery
-
-    with torch.no_grad():       
-        for img_tensors, labels in tqdm(val_dataloader, desc="Evaluating"):
-            labels = labels.cpu().numpy()
-            imgs = []
-
-            for img_tensor in img_tensors:
-                img_path = tensor_to_filepath(img_tensor)
-                img = Image.open(img_path).convert('RGB')
-                img = transforms.ToTensor()(img).unsqueeze(0).to(device)
-                imgs.append(img)
-
-            imgs = torch.cat(imgs, dim=0)
-
-            logits_per_image = model(imgs, gps_gallery)
-            probs = logits_per_image.softmax(dim=-1)
-
-            outs = torch.argmax(probs, dim=-1).detach().cpu().numpy()
-
-            preds.append(outs)
-            targets.append(labels)
-
-    preds = np.concatenate(preds, axis=0)
-    targets = np.concatenate(targets, axis=0)
-
-    model.train()
-
-    distance_thresholds = [2500, 750, 200, 25, 1]  # km
-    accuracy_results = {}
-    for dis in distance_thresholds:
-        acc, avg_distance_error = distance_accuracy(targets, preds, dis, gps_gallery)
-        print(f"Accuracy at {dis} km: {acc}, Average Distance Error: {avg_distance_error}")
-        accuracy_results[f'acc_{dis}_km'] = acc
-
-    return accuracy_results'''
